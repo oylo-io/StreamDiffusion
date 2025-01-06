c66048e864eea8e5e687060e65ff55768e42bb1b
himmelroman <himmelroman@gmail.com>
2025-01-07 00:16:00 +0200

    Support SDXL - Model Build

--- src/streamdiffusion/acceleration/tensorrt/models.py
+++ src/streamdiffusion/acceleration/tensorrt/models.py
@@ -53,11 +53,12 @@ class Optimizer:
 
     def infer_shapes(self, return_onnx=False):
         onnx_graph = gs.export_onnx(self.graph)
-        if onnx_graph.ByteSize() > 2147483648:
-            raise TypeError("ERROR: model size exceeds supported 2GB limit")
-        else:
-            onnx_graph = shape_inference.infer_shapes(onnx_graph)
+        # if onnx_graph.ByteSize() > 2147483648:
+        #     raise TypeError("ERROR: model size exceeds supported 2GB limit")
+        # else:
+        #     onnx_graph = shape_inference.infer_shapes(onnx_graph)
 
+        onnx_graph = shape_inference.infer_shapes(onnx_graph)
         self.graph = gs.import_onnx(onnx_graph)
         if return_onnx:
             return onnx_graph

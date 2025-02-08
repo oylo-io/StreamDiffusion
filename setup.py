8e6a730082ddb27fa715875ad731d1fc56625740
himmelroman <himmelroman@gmail.com>
2025-02-08 17:11:34 +0200

    Integrate compel

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ install_requires = [
     deps["diffusers"],
     deps["transformers"],
     deps["accelerate"],
-    "git+https://github.com/oylo-io/compel.git#egg=compel"
+    "compel @ git+https://github.com/oylo-io/compel.git"
 ]
 
 setup(
